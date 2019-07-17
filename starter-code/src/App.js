cd1889d8b8bf8401b525120103512b2af8fc7e6e
marianamv112 <marianamv112@gmail.com>
2019-07-17 10:59:40 +0100

    LIS Mariana Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import fullListOfContacts from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(fullListOfContacts.slice(0, 5));
+
+  function handleAddRandomContact() {
+    let randomIndex = Math.floor(Math.random() * fullListOfContacts.length);
+    let randomContact = fullListOfContacts[randomIndex];
+    setContacts([...contacts, randomContact]);
+  }
+
+  function handleSort(category) {
+    if (category === "name") {
+      setContacts(
+        [...contacts].sort((a, b) => {
+          if (a.name > b.name) return 1;
+          else return -1;
+        })
+      );
+    }
+    if (category === "popularity") {
+      setContacts(
+        [...contacts].sort((a, b) => {
+          if (a.popularity > b.popularity) return 1;
+          else return -1;
+        })
+      );
+    }
+  }
+
+  function handleDelete(indexToRemove) {
+    let sliced = [...contacts];
+    sliced.splice(indexToRemove, 1);
+    setContacts(sliced);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Iron Contacts</h1>
+      <div className="actions">
+        <button onClick={handleAddRandomContact}>Add Random</button>
+        <button onClick={() => handleSort("name")}>Sort by Name</button>
+        <button onClick={() => handleSort("popularity")}>
+          Sort by Popularity
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th> Picture</th>
+            <th> Name</th>
+            <th> Popularity</th>
+            <th> Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, i) => (
+            <tr key={i + contact.name}>
+              <td>
+                <img className="contact-picture" src={contact.pictureUrl} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td><button onClick={() => handleDelete(i)}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
